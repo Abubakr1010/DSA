3080bcb26c7e3825ccc0b104d801f7a990b13e30
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-03-03 19:57:23 +0100

    two pointer 1

--- two_pointer.py
+++ two_pointer.py
@@ -1,1 +1,15 @@
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if left[s] != right[s]:
+                return 2
+            left += 1
+            right -= 1
+
+        return 1
+
+
 
