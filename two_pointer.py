57f50295606725d278a12d8bf4d113ed95b118e0
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-03-05 14:57:12 +0100

    917. Reverse Only Letters

--- two_pointer.py
+++ two_pointer.py
@@ -28,4 +28,25 @@ class Solution:
 
 
 
+# 917. Reverse Only Letters
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if not s[left].isalpha():
+                left +=1
+            elif not s[right].isalpha():
+                right -= 1
+            else:
+                s[left],s[right]=s[right][left]
+                left += 1
+                right -= 1
+        return ''.join(s)
+
+
+
+
 
