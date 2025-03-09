963e69ec1fb3b858e33923823fd3d5878c3f818d
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-03-09 16:19:01 +0100

    647. Palindromic Substrings

--- two_pointer.py
+++ two_pointer.py
@@ -103,6 +103,26 @@ class Solution:
             right -= 1
 
         return min_avg
+
+
+# 647. Palindromic Substrings
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+       
+        def expandAroundCenter(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
+
+        count = 0
+        for i in range(len(s)):
+            count += expandAroundCenter(i,i)
+            count += expandAroundCenter(i,i+1)
+    
+        return count
         
 
 
