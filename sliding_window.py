6b959af76ff7963c4839cbf9a0ab1cc4532e6ddf
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-03-17 14:08:03 +0100

    219. Contains Duplicate II

--- sliding_window.py
+++ sliding_window.py
@@ -43,23 +43,43 @@
 # print (max_sum_sub_array(arr, k))
 
 
+
 # 209. Minimum Size Subarray Sum
+# class Solution:
+#     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+#         n = len(nums)
+#         left = 0
+#         min_value = float('inf')
+#         current_sum = 0
+
+#         for right in range(n):
+#             current_sum += nums[right]
+
+#             while current_sum >= target:
+#                 min_value = min(min_value, right - left + 1)
+#                 current_sum -= nums[left]
+#                 left += 1
+
+#         return min_value if min_value != float('inf') else 0
+
+
+
+
+# 219. Contains Duplicate II
 class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        n = len(nums)
-        left = 0
-        min_value = float('inf')
-        current_sum = 0
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
 
-        for right in range(n):
-            current_sum += nums[right]
+        for i in range(len(nums)):
+            if nums[i] in window:
+                return True
 
-            while current_sum >= target:
-                min_value = min(min_value, right - left + 1)
-                current_sum -= nums[left]
-                left += 1
+            window.add(nums[i])
 
-        return min_value if min_value != float('inf') else 0
+            if len(window) > k:
+                window.remove(nums[i - k])
+            
+        return False
 
 
 
