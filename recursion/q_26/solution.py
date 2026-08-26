aec19eebd6e363b79f931852d02f8a12df7adf24
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2026-08-26 20:15:27 +0200

    q-26-small-fix

--- recursion/q_26/solution.py
+++ recursion/q_26/solution.py
@@ -4,7 +4,7 @@
 from typing import Optional
 
 
-Definition for singly-linked list.
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
